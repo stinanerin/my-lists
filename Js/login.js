bd3ca5c66221ed2928b6a8ce63332664011b7f95
stormstina <stina.nerin@gmail.com>
2023-02-13 16:45:31 +0100

    Evenlistener function login form

--- Js/login.js
+++ Js/login.js
@@ -1,1 +1,22 @@
-console.log("hej");
+/* Immediately-invoked Function Expressions */
+(function() {
+
+
+    const loginForm = document.querySelector('#loginForm');
+    
+    // Submit-EventListener på form
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        // Hämtar valuet från users inlogg-försök
+        const loginEmail = document.querySelector('#loginEmail').value;
+        const loginPWD = document.querySelector('#loginPWD').value;
+
+
+    });
+
+
+
+})()
+
+
