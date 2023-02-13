e53a425571522c077cc4abff96a7556b18729274
stormstina <stina.nerin@gmail.com>
2023-02-13 18:22:24 +0100

    Global display none toggle function?

---
+++ Js/index.js
@@ -0,0 +1,3 @@
+
+// Förslag global funktion för att toggla mellan dispålay: none & display:block
+const toggleDisplay = element => element.style.display = element.style.display === "block" ? "none" : "block" ;
--- Js/login.js
+++ Js/login.js
@@ -143,7 +143,6 @@
     
     //     //todo! Byt vy till inloggnignsvyn
     
-    
     // });
 
 })()
