1734887a9269420fb019535aa55ec22215a23160
stormstina <stina.nerin@gmail.com>
2023-02-14 13:58:45 +0100

    Create and link rec.js file

--- Js/logout.js
+++ Js/logout.js
@@ -2,7 +2,6 @@
 
 // Ge alla knappar som ska rensa signend in user i local storage #logoutBtn som id
 const logoutBtn = document.querySelector("#logoutBtn");
-console.log(logoutBtn);
 
 logoutBtn && logoutBtn.addEventListener('click', (e) => {
     e.preventDefault();
